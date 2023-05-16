cc0fe91f1a597753ae4c9d1ff0d5d79b527e5043
vedantdave77 <vedantdave77@gmail.com>
2023-05-16 08:58:53 -0400

    change the test case code

--- test/aws-codepipeline-ts.test.ts
+++ test/aws-codepipeline-ts.test.ts
@@ -1,11 +1,13 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as CiCdAwsPipelineDemo from '../lib/ci-cd-aws-pipeline-demo-stack';
+import { handler } from '../lib/lambda/handler';
 
-import { handler } from "../lib/lambda/handler";
+describe('Lambda Function', () => {
+  it('should return a successful response', async () => {
+    const event = 'event data';
+    const context = 'context data';
 
-//https://docs.aws.amazon.com/codebuild/latest/userguide/test-reporting.html
-test('foo', async () => {
-    const result = await handler("", "");
-    expect(result.statusCode).toEqual(300);
+    const response = await handler(event, context);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe('Hello from a Lambda Function');
+  });
 });
