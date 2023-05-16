3d1d853560d8f055dcef158c7a6324cfc44eecb1
vedantdave77 <vedantdave77@gmail.com>
2023-05-16 10:35:28 -0400

    explaination

--- lib/aws-codepipeline-ts-stack.ts
+++ lib/aws-codepipeline-ts-stack.ts
@@ -11,21 +11,50 @@ export class AwsCodepipelineTsStack extends cdk.Stack {
     const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName: 'TestPipeline',
       synth: new ShellStep('Synth', {
+        // source
         input: CodePipelineSource.gitHub('vedantdave77/aws-codepipeline-ts', 'main'), //Remember to change 
+        // build
         commands: ['npm ci', 
                    'npm run build', 
                    'npx cdk synth']
       })
     });
 
+    /*
+    synth() --> it preform important tasks, and generate cloud formation template.
+      - dependency resolution --> ensure required library and modules are installed
+      - code generation --> analyze th app code, connstruct and configure the cloud formation template
+      - validation --> validate code and configurations
+      - output generation --> generate artifacts used further in deployment (cloud formation template)
+
+    update pipeline() --> make changes to ci/cd processes, (define the yaml or json file)
+     - addingn or removeing stages
+     - modifyin the actions
+     - adjusting the pipoene setting (name, artifact store, pipeline level configurationn)
+     - updatingn pipeie execution order
+    
+     - self mutate (means self update)
+
+    assets() --> used as custom stage
+     -- operation related to static assets and binary files.
+     - build or compilation
+     - asset processing
+     - artifact generation
+     - asset validation
+
+    */
+
+    //testing stage (lambda stack.prepare --> (mypipeline--> stage---> lambda_stack))
     const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
       env: { account: "398081196462", region: "ca-central-1" }
     }));
 
-
+    // testing stage (unit test)
     testingStage.addPre(new ShellStep("Run Unit Tests", { commands: ['npm install', 'npm test'] }));
+    // testing stage (manual approval)
     testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
-    
+
+    // prod stage
     const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
       env: { account: "398081196462", region: "ca-central-1" }
     }));
--- lib/lambda-stack.ts
+++ lib/lambda-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from 'constructs';
 import { Function, Runtime, Code, InlineCode } from 'aws-cdk-lib/aws-lambda';
 import * as path from "path";
 
-
+// (prepare --> deploy) in testing and prod environment
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, StageName: string, props?: cdk.StackProps) {
     super(scope, id, props);
